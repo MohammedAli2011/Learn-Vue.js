892122163e2bac163acf50821ea8c54a3c234c63
Mohammed Ali Abd ElShaheed <engineer.mohammed.ali11@gmail.com>
2026-06-18 11:33:42 +0300

    from lec1 to lec5

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ const { createApp } = Vue ;
 createApp({
     data () {
         return {
-            name: "Mohammed",
-            age: 15,
+            // name: "Mohammed",
+            // age: 15,
+            // link: "https://mohammed.com/",
+            // img: "./assets/images/mohammed.jpg",
+            num: 0,
         };
     },
     methods: {
-        sayHello (name) {
-            return `hello ${name}`;
+        // sayHello (name) {
+        //     return `hello ${name}`;
+        // },
+
+        clickFunc () {
+            this.num++
         }
     }
 }).mount("#parent")
