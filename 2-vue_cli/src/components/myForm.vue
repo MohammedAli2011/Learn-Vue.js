9c645b12345295e1cddc4f5474002150e1498479
Mohammed Ali Abd ElShaheed <engineer.mohammed.ali11@gmail.com>
2026-07-13 02:44:12 +0300

    lec 37

--- 2-vue_cli/src/components/myForm.vue
+++ 2-vue_cli/src/components/myForm.vue
@@ -1,29 +1,114 @@
 <template>
-  <div class="form">
-    <form action="">
-      <label for="normal"></label>
-      <input id="normal" type="text" />
+  <keep-alive>
+    <div class="form">
+      <form action="" @submit.prevent="makeStudent">
+        <!--        <label for="normal">normal</label>-->
+        <!--        <input id="normal" v-model="student.name_normal" type="text" />-->
 
-      <label for="lazy"></label>
-      <input id="lazy" type="text" />
+        <!--        <br />-->
+        <!--        {{ student.name_normal }}-->
+        <!--        <br />-->
 
-      <hr />
+        <!--        <label for="lazy">lazy</label>-->
+        <!--        <input id="lazy" v-model.lazy="student.name_lazy" type="text" />-->
 
-      <label for="male">Male</label>
-      <input id="male" name="" type="radio" value="male" />
+        <!--        <br />-->
+        <!--        {{ student.name_lazy }}-->
+        <!--        <br />-->
 
-      <label for="female">Female</label>
-      <input id="female" name="" type="radio" value="female" />
+        <!--        <hr />-->
 
-      <hr />
+        <!--        <label for="male">Male</label>-->
+        <!--        <input-->
+        <!--          id="male"-->
+        <!--          v-model="student.gender"-->
+        <!--          name=""-->
+        <!--          type="radio"-->
+        <!--          value="male"-->
+        <!--        />-->
 
-      <label for="football"></label>
-      <input id="football" name="" type="checkbox" />
+        <!--        <br />-->
+        <!--        {{ student.gender }}-->
+        <!--        <br />-->
 
-      <label for="tennis"></label>
-      <input id="tennis" name="" type="checkbox" />
-    </form>
-  </div>
+        <!--        <label for="female">Female</label>-->
+        <!--        <input-->
+        <!--          id="female"-->
+        <!--          v-model="student.gender"-->
+        <!--          name=""-->
+        <!--          type="radio"-->
+        <!--          value="female"-->
+        <!--        />-->
+
+        <!--        <hr />-->
+
+        <!--        <label for="football">Football</label>-->
+        <!--        <input-->
+        <!--          id="football"-->
+        <!--          v-model="student.sports.football"-->
+        <!--          name=""-->
+        <!--          type="checkbox"-->
+        <!--        />-->
+
+        <!--        {{ student.sports.football }}-->
+
+        <!--        <br />-->
+
+        <!--        <label for="tennis">Tennis</label>-->
+        <!--        <input-->
+        <!--          id="tennis"-->
+        <!--          v-model="student.sports.tennis"-->
+        <!--          name=""-->
+        <!--          type="checkbox"-->
+        <!--        />-->
+
+        <!--        <br />-->
+
+        <!--        {{ student.sports.tennis }}-->
+
+        <!--        <br />-->
+
+        <div>
+          <label for="">first name</label>
+          <input v-model="student.firstName" type="text" />
+        </div>
+
+        <br />
+
+        <div>
+          <label for="">last name</label>
+          <input id="" v-model="student.lastName" name="" type="text" />
+        </div>
+
+        <br />
+
+        <div>
+          <label for="">age</label>
+          <input id="" v-model="student.age" name="" type="number" />
+        </div>
+
+        <br />
+
+        <div>
+          <label for="">email</label>
+          <input id="" v-model="student.email" name="" type="email" />
+        </div>
+
+        <br />
+
+        <div>
+          <input id="" name="" type="submit" />
+        </div>
+        <ul>
+          <li v-for="(st, i) in this.students" :key="i">
+            <p>name is {{ st.name }}</p>
+            <p>age is : {{ st.age }}</p>
+            <p>email is : {{ st.email }}</p>
+          </li>
+        </ul>
+      </form>
+    </div>
+  </keep-alive>
 </template>
 
 <script>
@@ -31,11 +116,30 @@ export default {
   data() {
     return {
       student: {
-        gender: "",
-        sport: false,
+        // gender: "",
+        // name_lazy: "",
+        // name_normal: "",
+        // sports: {
+        //   football: false,
+        //   tennis: false,
+        // },
         name: "",
+        email: "",
+        age: "",
       },
+      students: [],
     };
+  },
+
+  methods: {
+    makeStudent() {
+      this.students.push(this.student);
+      this.student = {
+        name: "",
+        email: "",
+        age: "",
+      };
+    },
   },
 };
 </script>
--- 2-vue_cli/src/views/myMain.vue
+++ 2-vue_cli/src/views/myMain.vue
@@ -66,28 +66,187 @@
 <!--};-->
 <!--</script>-->
 
+<!--<template>-->
+<!--  <div class="myMain">-->
+<!--    <h1>this is my main page</h1>-->
+
+<!--    <br />-->
+<!--    <br />-->
+<!--    <br />-->
+
+<!--    <form action="" @submit.prevent="addStudent">-->
+<!--      <div>-->
+<!--        <label for="">first Name</label>-->
+<!--        <input v-model="user.firstName" type="text" />-->
+<!--      </div>-->
+
+<!--      <br />-->
+
+<!--      <div>-->
+<!--        <label for="">last Name</label>-->
+<!--        <input v-model="user.lastName" type="text" />-->
+<!--      </div>-->
+
+<!--      <br />-->
+
+<!--      <div>-->
+<!--        <label for="">age</label>-->
+<!--        <input id="" v-model="user.age" name="" type="number" />-->
+<!--      </div>-->
+
+<!--      <br />-->
+
+<!--      <div>-->
+<!--        <label for="">email</label>-->
+<!--        <input id="" v-model="user.email" name="" type="email" />-->
+<!--      </div>-->
+
+<!--      <br />-->
+
+<!--      <div>-->
+<!--        <label for="">role</label>-->
+<!--        <input id="" v-model="user.role" name="" type="text" />-->
+<!--      </div>-->
+
+<!--      <br />-->
+
+<!--      <div>-->
+<!--        <input id="" name="" type="submit" />-->
+<!--      </div>-->
+
+<!--      <ul style="list-style: none">-->
+<!--        <li v-for="user in this.users" :key="user.id">-->
+<!--          <hr />-->
+
+<!--          <div class="name">-->
+<!--            <h2 style="color: green">-->
+<!--              <span style="color: black">name is :</span>-->
+<!--              {{ user.firstName }}-->
+<!--              {{ user.lastName }}-->
+<!--            </h2>-->
+<!--          </div>-->
+
+<!--          <div-->
+<!--            class="id"-->
+<!--            style="display: flex; justify-content: start; margin-left: 25px"-->
+<!--          >-->
+<!--            <p style="color: green">-->
+<!--              <span style="color: black">id is:</span> {{ user.id }}-->
+<!--            </p>-->
+<!--          </div>-->
+
+<!--          <div-->
+<!--            class="age"-->
+<!--            style="display: flex; justify-content: start; margin-left: 25px"-->
+<!--          >-->
+<!--            <p style="color: green">-->
+<!--              <span class="" style="color: black"> age is : </span>-->
+<!--              {{ user.age }}-->
+<!--            </p>-->
+<!--          </div>-->
+
+<!--          <div-->
+<!--            class="roles"-->
+<!--            style="-->
+<!--              margin-left: 25px;-->
+<!--              display: flex;-->
+<!--              justify-content: start;-->
+<!--              align-items: center;-->
+<!--            "-->
+<!--          >-->
+<!--            <p-->
+<!--              class=""-->
+<!--              style="color: green; display: flex; justify-content: center"-->
+<!--            >-->
+<!--              <span style="color: black">role is:</span>-->
+<!--              <span>{{ user.role }}</span>-->
+<!--            </p>-->
+<!--          </div>-->
+
+<!--          <hr />-->
+<!--        </li>-->
+<!--      </ul>-->
+<!--    </form>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--export default {-->
+<!--  name: "myMain",-->
+
+<!--  components: {},-->
+
+<!--  data() {-->
+<!--    return {-->
+<!--      user: {-->
+<!--        firstName: "",-->
+<!--        lastName: "",-->
+<!--        email: "",-->
+<!--        age: "",-->
+<!--        role: "",-->
+<!--      },-->
+<!--      users: [],-->
+<!--    };-->
+<!--  },-->
+
+<!--  methods: {-->
+<!--    async getStudents() {-->
+<!--      localStorage.setItem("users", []);-->
+<!--      let users = localStorage.getItem("users");-->
+<!--      await fetch("https://dummyjson.com/users")-->
+<!--        .then((res) => res.json())-->
+<!--        .then((data) => (users = data.users));-->
+<!--      this.users = users;-->
+<!--      this.users === users;-->
+<!--    },-->
+
+<!--    async addStudent() {-->
+<!--      const dataRequest = {-->
+<!--        headers: { "content-type": "application/json" },-->
+<!--        method: "POST",-->
+<!--        body: JSON.stringify(this.user),-->
+<!--      };-->
+<!--      await fetch("https://dummyjson.com/users/add", dataRequest)-->
+<!--        .then((res) => res.json())-->
+<!--        .then((data) => this.users.push(data));-->
+<!--      this.user = {-->
+<!--        firstName: "",-->
+<!--        lastName: "",-->
+<!--        email: "",-->
+<!--        age: "",-->
+<!--        role: "",-->
+<!--      };-->
+<!--    },-->
+<!--  },-->
+
+<!--  async mounted() {-->
+<!--    await this.getStudents();-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
+
 <template>
-  <div class="myMain">
-    <h1>this is my main page</h1>
-    <my-form></my-form>
+  <h1>this is my main page</h1>
+
+  <div>
+    <div class="container">
+      <!--suppress VueUnrecognizedDirective -->
+      <p v-max-width="1000" class="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+        architecto, dignissimos, dolores est explicabo illo laboriosam magni
+        mollitia repudiandae sit soluta unde? A iure nobis qui quibusdam rem
+        temporibus voluptatum!
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import myForm from "./components/myForm";
-
-export default {
-  name: "myMain",
-  components: { myForm },
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
 h1 {
   color: green;
-}
-
-p {
-  color: #ddd;
 }
 </style>
