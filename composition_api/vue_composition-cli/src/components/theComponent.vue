d082ac757d5ae1199ebe08fbdd838f2964e98cb9
Mohammed Ali Abd ElShaheed <engineer.mohammed.ali11@gmail.com>
2026-08-01 15:26:36 +0300

    start composition api

---
+++ composition_api/vue_composition-cli/src/components/theComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>the name is {{ props.userName }} & age is {{ props.userAge }}</h1>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+let props = defineProps({
+  userName: {
+    type: String,
+    required: true,
+  },
+  userAge: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
---
+++ composition_api/vue_composition-cli/src/views/homePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--  <h1>Hello World</h1>-->
+
+  <!--  <p ref="fullName">I am {{ name }} {{ lastName }}</p>-->
+
+  <!--  <button @click="sayHello">say Hello</button>-->
+
+  <!--  <p>{{ incAge }}</p>-->
+
+  <!--  <input id="" v-model="age" name="" type="number" />-->
+
+  <TheComponent :userAge="userage" :userName="firstname" />
+</template>
+
+<script setup>
+import TheComponent from "@/components/theComponent.vue";
+import { ref } from "vue";
+//
+let firstname = ref("Mohammed");
+// let lastName = ref("Ali");
+let userage = ref(15);
+// let fullName = ref(1);
+// let incAge = computed(() => {
+//   return age.value + 5;
+// });
+//
+// setTimeout(() => {
+//   console.log(fullName.value);
+// }, 2000);
+//
+// function sayHello() {
+//   console.log("hello");
+// }
+</script>
